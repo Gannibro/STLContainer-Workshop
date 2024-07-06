60b09d7c3a28345a6cb67b2e473867b4577ee7dd
Aryaman Giri <aryamankgiri@gmail.com>
2024-07-06 10:49:14 +0545

    added required enums

--- P2/Flags.h
+++ P2/Flags.h
@@ -3,10 +3,12 @@
 
 namespace seneca
 {
+    // Options for displaying the files and directories
     enum class FormatFlags {
-        LONG,
+        LONG,   // to display in long format
     };
 
+    // Determine the type of filesystem resource
     enum class NodeType {
         DIR,
         FILE
